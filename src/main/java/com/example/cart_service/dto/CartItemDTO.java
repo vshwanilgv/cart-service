0daf353dadd18ee3d012533dcc16aa8c1b1c98d0
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-09-19 02:21:03 +0530

    Fixing issues

--- src/main/java/com/example/cart_service/dto/CartItemDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.cart_service.dto;
-
-public class CartItemDTO {
-    private Long productId;
-    private String productName;
-    private int quantity;
-    private double price;
-
-}
