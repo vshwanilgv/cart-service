03d699aee5d031685e5e6518f372c55616784dda
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-09-18 12:49:39 +0530

    Added cart , cart Item models and the cart DTO

--- src/main/java/com/example/cart_service/CartItemDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.cart_service;
-
-public class CartItemDTO {
-
-    private Long productId;
-    private int quantity;
-    private double price;
-
-}
---
+++ src/main/java/com/example/cart_service/model/Cart.java
@@ -0,0 +1,2 @@
+package com.example.cart_service.model;public class Cart {
+}
