5c7e9f752f2b47bdcd76a9c5dcb1cbdf4a01e63c
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-09-18 13:08:00 +0530

    Added changes to CartRepository

--- src/main/java/com/example/cart_service/repository/CartRepository.java
+++ src/main/java/com/example/cart_service/repository/CartRepository.java
@@ -1,10 +1,10 @@
 package com.example.cart_service.repository;
 
-import com.example.cart_service.model.CartItem;
+import com.example.cart_service.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CartRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findByProductId(Long productId);
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByUserId(Long userId);
 }
